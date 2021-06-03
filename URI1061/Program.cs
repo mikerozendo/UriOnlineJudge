e2219ffecb0a65c982c4e81bfbe05753f20b4e9a
Michael Rozendo <79727570+mikerozendo@users.noreply.github.com>
2021-06-03 15:36:08 -0300

    Add files via upload

---
+++ URI1061/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TreinoUri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> valores = new List<int>();
+            int primeiroValor = int.Parse(Console.ReadLine());
+            int segundoValor = int.Parse(Console.ReadLine());
+
+
+            int auxiliadora = 0;
+            if (primeiroValor > 0 && segundoValor > 0 && primeiroValor != segundoValor)
+            {
+
+
+                if (primeiroValor > segundoValor)
+                {
+
+                    auxiliadora = segundoValor + 1;
+
+                    if (auxiliadora % 5 == 2 || auxiliadora % 5 == 3)
+                    {
+
+                        valores.Add(auxiliadora);
+
+                    }
+
+                    while (primeiroValor != auxiliadora)
+                    {
+                        auxiliadora++;
+
+
+                        if (auxiliadora % 5 == 2 || auxiliadora % 5 == 3)
+                        {
+                            valores.Add(auxiliadora);                          
+
+                        }
+
+                    }
+
+                }
+
+
+                else if (segundoValor > primeiroValor)
+                {
+
+
+                    auxiliadora = segundoValor - 1;
+
+                    if (auxiliadora % 5 == 2 || auxiliadora % 5 == 3)
+                    {
+
+                        valores.Add(auxiliadora);
+
+                    }
+
+                    while (primeiroValor != auxiliadora)
+                    {
+                        auxiliadora--;
+
+
+                        if (auxiliadora % 5 == 2 || auxiliadora % 5 == 3)
+                        {
+                            valores.Add(auxiliadora);
+
+                        }
+                    }          
+                }
+
+
+
+                valores.RemoveAll(x => x == primeiroValor);
+                valores.RemoveAll(x => x == segundoValor);
+                valores.Sort();
+
+                foreach (int obj in valores)
+                {
+                    Console.WriteLine(obj);
+                }
+
+            }
+
+        }
+    }
+}
