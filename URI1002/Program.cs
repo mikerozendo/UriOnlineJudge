f9783e3c180f24511c3dc555204bf1169b4a832d
Michael Rozendo <79727570+mikerozendo@users.noreply.github.com>
2021-06-03 15:57:05 -0300

    1002

---
+++ URI1002/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = 3.14159;
+            double raio = double.Parse(Console.ReadLine());
+            double area = n * (Math.Pow(raio, 2));
+            Console.WriteLine("A=" + area.ToString("F4"));
+        }
+    }
+}
