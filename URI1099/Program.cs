72a4a0351f792185dc3be31d1f71795daba22a65
Michael Rozendo <79727570+mikerozendo@users.noreply.github.com>
2021-06-03 16:01:14 -0300

    1099

---
+++ URI1099/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace TreinoUri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int controlador = int.Parse(Console.ReadLine());
+            int soma = 0;
+
+
+            for (int i = 0; i < controlador; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+
+                int primeiroValor = int.Parse(values[0]);
+                int segundoValor = int.Parse(values[1]);
+
+
+
+                if (primeiroValor > segundoValor)
+                {
+                    if (primeiroValor - 1 != segundoValor)
+                    {
+                        while (primeiroValor - 1 != segundoValor)
+                        {
+                            primeiroValor -= 1;
+
+
+                            if (primeiroValor % 2 != 0)
+                            {
+                                soma += primeiroValor;
+                            }
+                        }
+                    }
+
+                    Console.WriteLine(soma);
+                    soma = 0;
+                }
+
+                else if (primeiroValor < segundoValor)
+                {
+
+                    if (primeiroValor + 1 != segundoValor)
+                    {
+
+                        while (primeiroValor + 1 != segundoValor)
+                        {
+                            primeiroValor += 1;
+
+
+                            if (primeiroValor % 2 != 0)
+                            {
+                                soma += primeiroValor;
+                            }
+                        }
+                    }
+
+                    Console.WriteLine(soma);
+                    soma = 0;
+                }
+
+                else if (primeiroValor == segundoValor)
+                {
+                    Console.WriteLine(soma);
+                }
+            }
+        }
+    }
+}
+
+
+
