07b614a56ef41ee880c039aa9f77a319eb756312
Michael Rozendo <79727570+mikerozendo@users.noreply.github.com>
2021-06-04 11:51:43 -0300

    1095

---
+++ URI1095/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Treino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeiraAuxiliar = 1;
+            int segundaAuxiliar = 60;
+
+            while (segundaAuxiliar != 0)
+            {
+                Console.WriteLine($"I={primeiraAuxiliar} J={segundaAuxiliar}");
+                primeiraAuxiliar += 3;
+                segundaAuxiliar -= 5;
+            }
+
+            Console.WriteLine($"I={primeiraAuxiliar} J={segundaAuxiliar}");
+        }
+    }
+}
