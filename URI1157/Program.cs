4941d258edb307afc21cbf0beca8a207fffa32bf
Michael Rozendo <79727570+mikerozendo@users.noreply.github.com>
2021-06-04 12:57:52 -0300

    1157

---
+++ URI1157/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Treino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i <= valor; i++)
+            {
+
+                if (valor % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
