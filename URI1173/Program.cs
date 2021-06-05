3bc50858fa8a6f1feed8187e6076f2a5696d41ed
Michael Rozendo <79727570+mikerozendo@users.noreply.github.com>
2021-06-05 18:37:48 -0300

    1173

---
+++ URI1173/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Treino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[10];
+
+
+            int valor = int.Parse(Console.ReadLine());
+
+
+            if (valor <= 50)
+            {
+
+
+                for (int i = 0; i < vetor.Count(); i++)
+                {
+
+                    vetor[i] = valor;
+                    valor += valor;
+
+                }
+
+
+                for (int j = 0; j < vetor.Count(); j++)
+                {
+
+                    Console.WriteLine($"N[{j}] = {vetor[j]}");
+
+                }
+            }
+        }
+    }
+}
