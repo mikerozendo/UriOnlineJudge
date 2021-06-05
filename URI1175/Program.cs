24acddcab97965f973e43caef219fab7ed3512a2
Michael Rozendo <79727570+mikerozendo@users.noreply.github.com>
2021-06-05 17:36:05 -0300

    1175

---
+++ URI1175/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace Treino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Vetor = new int[20];
+            int controladora = 19;
+            int auxiliadora;
+
+
+
+            for (int i = 0; i < Vetor.Count(); i++)
+            {
+
+                Vetor[i] = int.Parse(Console.ReadLine());
+
+            }
+
+
+
+            for (int j = 0; j < Vetor.Count(); j++)
+            {
+
+                if (controladora >= 10)
+                {
+
+                    auxiliadora = Vetor[controladora];
+                    Vetor[controladora] = Vetor[j];
+
+                    Vetor[j] = auxiliadora;
+                    controladora--;
+
+                }
+            }
+
+
+
+            for (int k = 0; k < Vetor.Count(); k++)
+            {
+                Console.WriteLine($"N[{k}] = {Vetor[k]}");
+            }
+
+        }
+    }
+}
