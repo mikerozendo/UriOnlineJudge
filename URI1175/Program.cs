196607e03df7164ad18664ef504bbde6372c17c2
Michael Rozendo <79727570+mikerozendo@users.noreply.github.com>
2021-06-05 17:32:09 -0300

    1175

---
+++ URI1175/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UriOnline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int repeticao = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < repeticao; i++)
+            {
+                if (i <= repeticao - 2)
+                {
+                    Console.Write("Ho ");
+                }
+
+                if (i == repeticao - 1)
+                {
+                    Console.Write("Ho");
+                }
+
+                if (i == repeticao - 1)
+                {
+                    Console.Write("!");
+                }
+            }
+
+
+
+        }
+    }
+}
+
