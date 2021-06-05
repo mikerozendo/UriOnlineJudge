504d7215d398d399d87fd89ff8c350f05f8cc989
Michael Rozendo <79727570+mikerozendo@users.noreply.github.com>
2021-06-05 18:03:18 -0300

    1174

---
+++ URI1174/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Globalization;
+
+namespace Treino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] vetor = new double[100];
+
+
+            for (int i = 0; i < vetor.Count(); i++)
+            {
+
+                vetor[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            }
+
+
+            for (int i = 0; i < vetor.Count(); i++)
+            {
+
+                if (vetor[i] <= 10)
+                {
+
+                    Console.WriteLine($"A[{i}] = {vetor[i].ToString("F1", CultureInfo.InvariantCulture)}");
+                }
+
+            }
+        }
+    }
+}
