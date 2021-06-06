70354e0f8ee572a0c2bfb0ad2c0ce0f97baf3b2f
Michael Rozendo <79727570+mikerozendo@users.noreply.github.com>
2021-06-06 16:18:50 -0300

    1159

---
+++ URI1159/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace Treino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorEntrada = int.Parse(Console.ReadLine());
+            int soma = 0;
+
+            int controlador = 0;
+
+            while (valorEntrada != 0)
+            {
+
+                while (controlador != 5)
+                {
+
+                    if (valorEntrada % 2 == 0)
+                    {
+                        soma += valorEntrada;
+                        controlador += 1;
+                    }
+
+                    valorEntrada += 1;
+                }
+
+
+                Console.WriteLine(soma);
+                valorEntrada = int.Parse(Console.ReadLine());
+
+                soma = 0;
+                controlador = 0;
+            }
+        }
+    }
+}
