bf26542314581abbf8832c35cc321ea0129b66ec
Michael Rozendo <79727570+mikerozendo@users.noreply.github.com>
2021-06-07 13:15:17 -0300

    1177

---
+++ URI1177/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace Treino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorControle = int.Parse(Console.ReadLine());
+
+            int[] vetor = new int[1000];
+            int contador = 0;
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+
+
+                if (contador == valorControle)
+                {
+
+                    contador = 0;
+                    vetor[i] = contador;
+                    contador += 1;
+
+                }
+
+                else
+                {
+                    vetor[i] = contador;
+                    contador += 1;
+                }
+            }
+
+            for (int j = 0; j < vetor.Length; j++)
+            {
+                Console.WriteLine($"N[{j}] = {vetor[j]}");
+                
+            }
+        }
+    }
+}
