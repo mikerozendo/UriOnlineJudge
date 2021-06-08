ba2bcf417e77451433eb98bcdf51d2124d9833ed
Michael Rozendo <79727570+mikerozendo@users.noreply.github.com>
2021-06-08 18:02:34 -0300

    1047

---
+++ URI1047/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Treino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] values = Console.ReadLine().Split(' ');
+
+
+            int horaInicial = int.Parse(values[0]);
+            int minutoInicial = int.Parse(values[1]);
+            int horaFinal = int.Parse(values[2]);
+            int minutoFinal = int.Parse(values[3]);
+
+
+
+            if (horaInicial == minutoInicial &&
+                horaInicial == minutoFinal &&
+                horaInicial == horaFinal &&
+                horaFinal == minutoFinal &&
+                horaFinal == minutoInicial)
+            {
+
+                DateTime inicio = new DateTime(1, 1, 1, horaInicial, 0, 0);
+                DateTime fim = new DateTime(1, 1, 2, horaInicial, 0, 0);
+
+                TimeSpan duracao = fim - inicio;
+
+                Console.WriteLine($"O JOGO DUROU {duracao.TotalHours} HORA(S) E {duracao.Minutes} MINUTO(S)");
+            }
+
+
+
+            else if (horaFinal > horaInicial)
+            {
+
+                DateTime inicio = new DateTime(1, 1, 1, horaInicial, minutoInicial, 0);
+                DateTime fim = new DateTime(1, 1, 1, horaFinal, minutoFinal, 0);
+
+                TimeSpan duracao = fim - inicio;
+
+                Console.WriteLine($"O JOGO DUROU {duracao.Hours} HORA(S) E {duracao.Minutes} MINUTO(S)");
+            }
+
+
+            else 
+            {
+
+                DateTime inicio = new DateTime(1, 1, 1, horaInicial, minutoInicial, 0);
+                DateTime fim = new DateTime(1, 1, 2, horaFinal, minutoFinal, 0);
+
+                TimeSpan duracao = fim - inicio;
+
+                Console.WriteLine($"O JOGO DUROU {duracao.Hours} HORA(S) E {duracao.Minutes} MINUTO(S)");
+            }
+        }
+
+    }
+}
+
