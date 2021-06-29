e191a3339aa812e2aefa4c7dc3642ded86adf3df
Michael Rozendo <79727570+mikerozendo@users.noreply.github.com>
2021-06-29 13:21:03 -0300

    1164
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1164

---
+++ URI1164/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+
+namespace UriOnlineJudge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int controlador = int.Parse(Console.ReadLine());
+            int[] vetor = new int[controlador];
+
+            int divisora = 1;
+            int auxiliadoraSoma = 0;
+
+
+
+            for (int i = 0; i < controlador; i++)
+            {
+
+                vetor[i] = int.Parse(Console.ReadLine());
+
+
+
+                for (int j = vetor[i]; j >= 1; j--)
+                {
+
+
+                    if (vetor[i] % divisora == 0 && divisora != vetor[i])
+                    {
+
+                        auxiliadoraSoma += divisora;
+
+                    }
+
+
+                    divisora++;
+
+
+
+                    if (j == 1 && auxiliadoraSoma == vetor[i])
+                    {
+                        Console.WriteLine($"{vetor[i]} eh perfeito");
+                        divisora = 1;
+                        auxiliadoraSoma = 0;
+                    }
+
+
+                    else if (j == 1 && auxiliadoraSoma != vetor[i])
+                    {
+                        Console.WriteLine($"{vetor[i]} nao eh perfeito");
+                        divisora = 1;
+                        auxiliadoraSoma = 0;
+                    }
+                }
+            }
+        }
+    }
+}
