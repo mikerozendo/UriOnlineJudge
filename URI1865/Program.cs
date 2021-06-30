f10ef2e2605fa2a8bd2dcf7de2597f98c8a29cc7
Michael Rozendo <79727570+mikerozendo@users.noreply.github.com>
2021-06-30 15:29:55 -0300

    1865

---
+++ URI1865/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace UriOnlineJudge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int controlador = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < controlador; i++)
+            {
+
+                string[] value = Console.ReadLine().Split(' ');
+
+                string personagem = value[0];
+                int newtons = int.Parse(value[1]);
+
+
+                if (personagem == "Thor")
+                {
+                    Console.WriteLine("Y");
+                }
+
+
+                else
+                {
+                    Console.WriteLine("N");
+                }
+            }
+        }
+    }
+}
