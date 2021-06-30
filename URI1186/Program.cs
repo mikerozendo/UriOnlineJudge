553c8fa29567836f0233a96cf24a0b4e03cda832
Michael Rozendo <79727570+mikerozendo@users.noreply.github.com>
2021-06-30 17:02:44 -0300

    1186

---
+++ URI1186/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+
+namespace UriOnlineJudge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[,] matriz = new double[12, 12];
+
+
+            char validacao = char.Parse(Console.ReadLine());
+
+
+            double soma = 0;
+            int contador = 11;
+
+
+            //Lendo valores da matriz 
+            for (int i = 0; i < 12; i++)
+            {
+
+                for (int j = 0; j < 12; j++)
+                {
+                    matriz[i, j] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                }
+
+            }
+
+
+            //Filtrando valores
+            for (int k = 1; k < 12; k++)
+            {
+
+                for (int f = 11; f >= contador; f--)
+                {
+                    soma += matriz[k , f];
+                }
+
+                contador--;
+            }
+
+
+
+            //Calculo mediante a validacao
+            if (validacao == 'S')
+            {
+
+                Console.WriteLine(soma.ToString("F1", CultureInfo.InvariantCulture));
+
+            }
+
+
+            else if (validacao == 'M')
+            {
+
+                double media = soma / 66;
+                Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
+
+            }
+        }
+    }
+}
